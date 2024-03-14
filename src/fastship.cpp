17b1278f2f56a3abbecce8b0b70b6c727abb0812
ataolmez <a.olmez@etu.edu.tr>
2024-03-14 16:16:28 +0300

    cpp files

--- src/fastship.cpp
+++ src/fastship.cpp
@@ -1,3 +1,37 @@
 //
 // Created by ATA on 14.03.2024.
 //
+#include <iostream>
+#include "../inc/fastship.h"
+
+
+
+void fastship::DecreaseHealth(int damage) {
+    health = health - damage*damagemodifier;
+    if (health < 0) {
+        health = 0;
+    }
+}
+
+void fastship::DecreaseFuel() {
+    fuel = fuel - 33;
+}
+double fastship::GetHealth() {
+    return health;
+}
+int fastship::GetFuel() const {
+    return fuel;
+}
+int fastship::GetMoney() {
+    return money;
+}
+void fastship::changeMoney(int pay) {
+    money = money + pay;
+}
+double fastship::GetChance() const {
+    return chancemodifier;
+}
+double fastship::GetDamage() const {
+    return damagemodifier;
+}
+
--- src/normalship.cpp
+++ src/normalship.cpp
@@ -1,3 +1,35 @@
 //
 // Created by ATA on 14.03.2024.
 //
+#include <iostream>
+#include "../inc/normalship.h"
+
+
+
+void normalship::DecreaseHealth(int damage) {
+    health = health - damage*damagemodifier;
+    if (health < 0) {
+        health = 0;
+    }
+}
+void normalship::DecreaseFuel() {
+    fuel = fuel - 33;
+}
+double normalship::GetHealth() {
+    return health;
+}
+int normalship::GetFuel() const {
+    return fuel;
+}
+int normalship::GetMoney()  {
+    return money;
+}
+void normalship::changeMoney(int pay) {
+    money = money + pay;
+}
+double normalship::GetChance() const {
+    return chancemodifier;
+}
+double normalship::GetDamage() const {
+    return damagemodifier;
+}
