23d4645ea1756845dd8003edfcb79eedba9b8be9
ataolmez <a.olmez@etu.edu.tr>
2024-03-14 16:31:22 +0300

    sum

--- inc/normalship.h
+++ inc/normalship.h
@@ -10,7 +10,10 @@ class normalship:public Ship {
 
 public:
 
-    normalship(int _fuel, int _money, double _health, double _damagemodifier, double _chancemodifier) : Ship(_fuel,_money,_health,_damagemodifier,_chancemodifier) {}
+    normalship(int _fuel, int _money, double _health, double _damagemodifier, double _chancemodifier) : Ship(_fuel,_money,_health,_damagemodifier,_chancemodifier) {
+        std::cout<<"normalship called"<<"\n";
+    }
+    ~normalship() override{std::cout<<"normalship destructor called"<<"\n";};
 
     void DecreaseHealth(int damage) override;
     void DecreaseFuel() override;
--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
 //
 // Created by ATA on 14.03.2024.
 //
+#include <iostream>
+#include "inc/Ship.h"
+#include "inc/fastship.h"
+#include "inc/normalship.h"
+#include "inc/strongship.h"
 
 int main(){
-return 0;
+    normalship obj(100,100,100,1,1);
+    std::cout<< obj.GetDamage()<<"\n";
+    
+    return 0;
 }
